6f33f8f1ac88da1dd3bab83b21ef7a0a3ed0a8eb
Lolz <92547513+AwesomePotato23@users.noreply.github.com>
2021-12-20 10:26:44 -0600

    basic template added

---
+++ set-up/boot.py
@@ -0,0 +1,15 @@
+import os
+import time
+import random
+import setup.py
+
+###########
+# Pything #
+# Boot up #
+#   1.0   #
+###########
+
+# Programmed by @AwesomePotato22 on Replit
+
+print("Loading...")
+time.sleep(8.5)
