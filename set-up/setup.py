21a7e063ece556e4fd4322fdd4c4d1a7d22b26cb
Lolz <92547513+AwesomePotato23@users.noreply.github.com>
2021-12-20 10:33:06 -0600

    created simple setup

--- set-up/setup.py
+++ set-up/setup.py
@@ -5,3 +5,14 @@ print("Welcome to the Pything setup!")
 time.sleep
 print("")
 print(" == SETUP MENU == ")
+print("Welcome to Pything!")
+print("Please answer these Qs.")
+
+username = input("Enter a name: ")
+print("Welcome, " + username + "!")
+password = input("Make a password: ")
+if password == "" or password == " ":
+  print("PLEASE ENTER A PASSWORD NEXT TIME.")
+  quit()
+else:
+  import survey
