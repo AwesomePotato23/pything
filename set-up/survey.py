e4642ac197899bb09c44400bce8d75841e73f87e
Lolz <92547513+AwesomePotato23@users.noreply.github.com>
2021-12-20 10:39:11 -0600

    survey added

---
+++ set-up/survey.py
@@ -0,0 +1,14 @@
+import time
+print("Last but not least, I need some information.")
+print("This will not be stored and will be deleted after stopping program.")
+
+print("== SURVEY SCREEN ==")
+gender = input("What is your gender: ")
+favColor = input("What is your favorite color: ")
+
+# Might need to add some more questions...
+
+print("If I am correct, you are a " + gender + ", and your favorite color is" + favColor +".")
+print("Thank you for completing the setup, now proceeding to enter PyThing terminal.")
+
+import pything
